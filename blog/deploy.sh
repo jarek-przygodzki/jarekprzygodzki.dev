859e556afa0d23fbce49d9a6ffb6b1e585695392
Jarek Przygódzki <jarek.przygodzki@gmail.com>
2019-06-21 22:49:30 +0200

    Improve deploy.sh

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -4,6 +4,13 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+(
+    cd public &&
+    git fetch && 
+    git checkout -f master &&
+    git reset --hard origin/master
+)
+
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
